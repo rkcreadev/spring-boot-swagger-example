0ad3fd5d184712924949780d5c75976a59090333
r.kutumbaev <r.kutumbaev@instabia.ru>
2018-02-07 19:12:44 +0700

    add spring data jpa and separate swagger configs

--- petstore/src/main/java/com/rkcreadev/demo/petstore/PetStoreApplication.java
+++ petstore/src/main/java/com/rkcreadev/demo/petstore/PetStoreApplication.java
@@ -1,19 +1,22 @@
 package com.rkcreadev.demo.petstore;
 
+import com.rkcreadev.demo.petstore.api.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class PetStoreApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(PetStoreApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PetStoreApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetStoreApplication.class, args);
+    }
 }
